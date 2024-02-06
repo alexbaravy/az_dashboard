c531a6273812ae0aa4ef52a6de81c0a680b1114d
Alex <alexbaravy@gmail.com>
2024-02-06 17:17:13 +0300

    refactoring to classes. modificated bot

--- vristo-django/main/tasks.py
+++ vristo-django/main/tasks.py
@@ -2,34 +2,36 @@ from celery import shared_task
 from django.conf import settings
 from django.utils import timezone
 from .models import CDN, Hosting, Domain, Website
-from .utils import send_notification, check_website, notify_services
-from django.contrib.auth.models import User
+from .utils import ExpirationNotifier, TelegramNotifier, WebsiteChecker
+import os
 
+notifier = TelegramNotifier(token=os.environ.get('BOT_TOKEN'), chat_id=os.environ.get('CHAT_ID'))
 
 @shared_task
 def get_websites():
+    checker = WebsiteChecker(notifier=notifier, az_verification_tag=os.environ.get('AZ_VERIFICATION_TAG'))
+
     websites = Website.objects.filter(
         check_enabled=True,
         deactivated=False
     ).values('id', 'domain__url', 'domain_hash')
 
     for website in websites:
-        check_website(website['id'], website['domain__url'], website['domain_hash'])
+        checker.check_website(website['id'], website['domain__url'], website['domain_hash'])
 
 
 @shared_task
 def get_expiration_services():
+    expiration_notifier = ExpirationNotifier(notifier=notifier)
+
     now = timezone.now()
     days_later = now + timezone.timedelta(days=10)
 
-    domain_services = Domain.objects.filter(end_date__lt=days_later, check_enabled=True,
-                                            deactivated=False).values('url', 'end_date')
-    notify_services(domain_services, 'Domain')
+    services_to_check = [
+        (Domain, 'url'),
+        (Hosting, 'ip'),
+        (CDN, 'ip')
+    ]
 
-    hosting_services = Hosting.objects.filter(end_date__lt=days_later, check_enabled=True,
-                                              deactivated=False).values('ip', 'end_date')
-    notify_services(hosting_services, 'Hosting')
-
-    cdn_services = CDN.objects.filter(end_date__lt=days_later, check_enabled=True,
-                                      deactivated=False).values('ip', 'end_date')
-    notify_services(cdn_services, 'CDN')
+    for model, field_name in services_to_check:
+        expiration_notifier.check_services(model, field_name, days_later)
--- vristo-django/main/utils.py
+++ vristo-django/main/utils.py
@@ -1,71 +1,95 @@
+from typing import Optional
 import telebot
 import logging
 import requests
 from bs4 import BeautifulSoup
 from django.utils import timezone
 from .models import Website, UnavailableLog
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from django.db.models import QuerySet
 
 logger = logging.getLogger(__name__)
 
-# telegram API keys
-bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
-chat_id = os.environ.get('CHAT_ID')
 
-az_verification_tag = os.environ.get('AZ_VERIFICATION_TAG')
+class TelegramNotifier:
+    def __init__(self, token: str, chat_id: str):
+        self.bot = telebot.TeleBot(token)
+        self.chat_id = chat_id
 
-
-def send_notification(result):
-    bot.send_message(chat_id, result, disable_web_page_preview=True)
-    logger.info(f"Notification: {result}")
-
-
-def send_unavailable_log(id, response):
-    website = Website.objects.get(id=id)
-    latest_issue = website.unavailablelog_set.order_by('-id').first()
-    if latest_issue is None or (latest_issue.start_status != response.status_code):
-        UnavailableLog.objects.filter(website_id=id, end_date=None).update(
-            end_date=timezone.now(),
-            end_status=response.status_code
-        )
-
-        UnavailableLog.objects.create(
-            website=website,
-            end_date=None,
-            start_status=response.status_code
-        )
+    def send_notification(self, result: str):
+        self.bot.send_message(self.chat_id, result, disable_web_page_preview=True)
+        logger.info(f"Notification: {result}")
 
 
-def check_website(id, url, domain_hash):
-    try:
-        response = requests.get(url)
+class WebsiteChecker:
+    def __init__(self, notifier: TelegramNotifier, az_verification_tag: str):
+        self.notifier = notifier
+        self.az_verification_tag = az_verification_tag
 
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            meta_tag = soup.find('meta', {'name': az_verification_tag})
-
-            if meta_tag and meta_tag.get('content') == domain_hash:
-                result = f'{url}: OK'
+    def check_website(self, id: int, url: str, domain_hash: str) -> Optional[str]:
+        result = None
+        response = None
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                result = self._process_successful_response(id, url, domain_hash, response)
             else:
-                result = f"{url}: Error: Tag 'az-verification' not found or hash value does not match."
-                response.status_code = 299
+                result = f"{url}: Error: HTTP {response.status_code}"
+        except requests.RequestException as e:
+            result = f"{url}: Error: {str(e)}"
+        finally:
+            if result:
+                self.notifier.send_notification(result)
+            if response:
+                self._send_unavailable_log(id, response)
+            return result
+
+    def _process_successful_response(self, id: int, url: str, domain_hash: str, response: requests.Response) -> str:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        meta_tag = soup.find('meta', {'name': self.az_verification_tag})
+        website = Website.objects.get(id=id)
+        latest_issue = website.unavailablelog_set.order_by('-id').first()
+        if meta_tag and meta_tag.get('content') == domain_hash:
+            if latest_issue and latest_issue.start_status != 200:
+                return f'{url}: OK'
         else:
-            result = f"{url}: Error: HTTP {response.status_code}"
+            response.status_code = 299
+            return f"{url}: Error: Tag '{self.az_verification_tag}' not found or hash value does not match."
 
-    except requests.RequestException as e:
-        result = f"{url}: Error: {str(e)}"
-    finally:
-        send_notification(result)
-        send_unavailable_log(id, response)
-        return result
+    def _send_unavailable_log(self, id: int, response: requests.Response):
+        website = Website.objects.get(id=id)
+        latest_issue = website.unavailablelog_set.order_by('-id').first()
+        if latest_issue is None or (latest_issue.start_status != response.status_code):
+            UnavailableLog.objects.filter(
+                website_id=id,
+                end_date=None
+            ).update(
+                end_date=timezone.now(),
+                end_status=response.status_code
+            )
+
+            UnavailableLog.objects.create(
+                website=website,
+                end_date=None,
+                start_status=response.status_code
+            )
 
 
-def notify_services(services, service_name):
-    for service in services:
-        service_type = 'url' if 'url' in service else 'ip'
-        result = f"{service_name}: {service[service_type]}. Expiration Date: {service['end_date']}"
-        send_notification(result)
-        return result
+class ExpirationNotifier:
+    def __init__(self, notifier: TelegramNotifier):
+        self.notifier = notifier
+
+    def check_services(self, model: type, field_name: str, end_date: timezone.datetime):
+        services = model.objects.filter(
+            end_date__lt=end_date,
+            check_enabled=True,
+            deactivated=False
+        ).values(field_name, 'end_date')
+
+        if isinstance(services, QuerySet) and services.exists():
+            self.notify_expiration_services(services, model.__name__)
+
+    def notify_expiration_services(self, services, service_name: str):
+        for service in services:
+            service_type = 'url' if 'url' in service else 'ip'
+            result = f"{service_name}: {service[service_type]}. Expiration Date: {service['end_date']}"
+            self.notifier.send_notification(result)
